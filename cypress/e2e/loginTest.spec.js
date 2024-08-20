1abd5a506f72e5c0433b9ef3473248c3425cbc64
Abolfazl Amuzegari <abolfazlamuzegari@gmail.com>
2024-08-21 03:27:25 +0330

    refactored visisting URL happening one time

--- cypress/e2e/loginTest.spec.js
+++ cypress/e2e/loginTest.spec.js
@@ -11,11 +11,10 @@ import {
 describe("login", () => {
   beforeEach(() => {
     cy.signUp(Email, firstName, lastName, phoneNumber, birthdate, password);
+    cy.visit("/login/login.html");
   });
 
   it("testing the login pop up alert", () => {
-    cy.visit("/login/login.html");
-
     cy.get("#login-email").type(Email);
     cy.get("#login-password").type(password);
     cy.get("#login-form").submit();
@@ -27,25 +26,21 @@ describe("login", () => {
   });
 
   it("testing mandatory fields for login page", () => {
-    cy.visit("/login/login.html");
     cy.get("#login-email").should("have.attr", "required");
     cy.get("#login-password").should("have.attr", "required");
   });
   it("should not submit login if mandatory fields are empty", () => {
-    cy.visit("/login/login.html");
     cy.get("button").click();
     cy.get("#login-email:invalid").should("exist");
     cy.get("#login-password:invalid").should("exist");
   });
   it("testing if the link button redirects back to signup page", () => {
-    cy.visit("/login/login.html");
     cy.contains("Sign Up").click();
     cy.url().should("not.include", "/login.html");
     cy.url().should("include", "/signup.html");
   });
 
   it("testing the failed login", () => {
-    cy.visit("/login/login.html");
     cy.get("#login-email").type("notTest@test.com");
     cy.get("#login-password").type("not1");
     cy.get("#login-form").submit();
